117027a6908c56d9e1c5c3bae075a4c944d723ce
Dana Pham <danapham12@gmail.com>
2021-02-04 18:18:56 -0600

    starting exercises

---
+++ CollectionsAndLinqExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsAndLinqExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
