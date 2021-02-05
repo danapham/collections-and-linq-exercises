ecbe293edae52ba6ede602b5ab45777056554488
Dana Pham <danapham12@gmail.com>
2021-02-04 20:11:14 -0600

    completed exercise

--- CollectionsAndLinqExercises/Program.cs
+++ CollectionsAndLinqExercises/Program.cs
@@ -7,15 +7,22 @@ namespace CollectionsAndLinqExercises
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            var numbers = new List<int>();
-            for (var i = 0; i < 20; i++)
+            var myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("mother", new Dictionary<string, string>() { { "name", "Mai" }, { "age", "63" } });
+            myFamily.Add("father", new Dictionary<string, string>() { { "name", "Danh" }, { "age", "70" } });
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "David" }, { "age", "22" } });
+
+            foreach ((string member, Dictionary<string, string> info) in myFamily)
             {
-            numbers.Add(random.Next(1, 50));
+
+                if (info.TryGetValue("name", out string name))
+                {
+                    if (info.TryGetValue("age", out string age))
+                    {
+                        Console.WriteLine($"{name} is my {member} and is {age} years old");
+                    }
+                }
             }
-            var squared = new List<int>();
-            numbers.ForEach(number => squared.Add(number * number));
-            squared.RemoveAll(number => number % 2 != 0);
         }
     }
 }
