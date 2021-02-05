1903ed96e61fb19513c4fda51affe86803f49995
Dana Pham <danapham12@gmail.com>
2021-02-04 21:28:56 -0600

    saving

--- CollectionsAndLinqExercises/Program.cs
+++ CollectionsAndLinqExercises/Program.cs
@@ -7,15 +7,40 @@ namespace CollectionsAndLinqExercises
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            var numbers = new List<int>();
-            for (var i = 0; i < 20; i++)
+            var stocks = new Dictionary<string, string>();
+            stocks.Add("GME", "Gamestop");
+            stocks.Add("BRK-A", "Berkshire Hathaway");
+            stocks.Add("TSLA", "Tesla");
+            stocks.Add("DIS", "Walt Disney");
+
+            List<(string ticker, int shares, double price)> purchases = new List<(string, int, double)>();
+
+            purchases.Add((ticker: "GME", shares: 500, price: 53.50));
+            purchases.Add((ticker: "BRK-A", shares: 1, price: 354760));
+            purchases.Add((ticker: "TSLA", shares: 10, price: 849.99));
+            purchases.Add((ticker: "DIS", shares: 50, price: 180.23));
+            purchases.Add((ticker: "GME", shares: 500, price: 53.50));
+
+            var purchaseInfo = new Dictionary<string, double>();
+            foreach ((string ticker, int shares, double price) purchase in purchases)
             {
-            numbers.Add(random.Next(1, 50));
+                if (purchaseInfo.TryGetValue(purchase.ticker, out double valuation))
+                {
+                    purchaseInfo[purchase.ticker] = valuation += (purchase.shares * purchase.price);
+                }
+                else
+                {
+                    purchaseInfo.Add(purchase.ticker, purchase.shares * purchase.price);
+                }
             }
-            var squared = new List<int>();
-            numbers.ForEach(number => squared.Add(number * number));
-            squared.RemoveAll(number => number % 2 != 0);
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Total Ownership Report");
+            foreach ((string company, double valuation) in purchaseInfo)
+            {
+                Console.WriteLine($"{company}: ${valuation}");
+            }
+            Console.WriteLine("----------------------------");
         }
     }
 }
