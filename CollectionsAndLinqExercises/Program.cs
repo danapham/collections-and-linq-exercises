4a2fd44c0e42109f98f6d29503d6278ef0221e26
Dana Pham <67748110+danapham@users.noreply.github.com>
2021-02-04 18:37:18 -0600

    Revert "completed exercise"

--- CollectionsAndLinqExercises/Program.cs
+++ CollectionsAndLinqExercises/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CollectionsAndLinqExercises
 {
@@ -7,15 +6,7 @@ namespace CollectionsAndLinqExercises
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            var numbers = new List<int>();
-            for (var i = 0; i < 20; i++)
-            {
-            numbers.Add(random.Next(1, 50));
-            }
-            var squared = new List<int>();
-            numbers.ForEach(number => squared.Add(number * number));
-            squared.RemoveAll(number => number % 2 != 0);
+            Console.WriteLine("Hello World!");
         }
     }
 }
